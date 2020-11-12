59ac2110a22f217ce95186e625f48372da96e56d
charla-07 <karlytaframa@hotmail.com>
2020-11-12 08:29:41 -0600

    first commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -7,6 +7,50 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
+  pictures: any[] = [
+    {id: 1, url: '../assets/images/inicio1.jpg'},
+    {id: 2, url: '../assets/images/inicio2.jpg'},
+  ]
+  slideOpts = {
+    initialSlide: 1,
+    speed: 400,
+    slidesPerView: 2.5,
+    spaceBetween: 30 
+  };
+  
+  purses: any[] = [
+    {id: 1, url: '../assets/images/slider1.png'},
+    {id: 2, url: '../assets/images/slider2.png'},
+    {id: 3, url: '../assets/images/slider1.png'},
+    {id: 4, url: '../assets/images/slider2.png'},
+    {id: 5, url: '../assets/images/slider1.png'},
+    {id: 6, url: '../assets/images/slider2.png'},
+  ]
+
+  pictures2: any[] = [
+    {id: 1, url: '../assets/images/inicio3.jpg'},
+    {id: 2, url: '../assets/images/inicio4.jpg'},
+    {id: 3, url: '../assets/images/inicio5.jpg'},
+    {id: 4, url: '../assets/images/inicio6.jpg'},
+    {id: 5, url: '../assets/images/inicio7.jpg'},
+    {id: 6, url: '../assets/images/inicio8.jpg'},
+  ]
+
+  purses2: any[] = [
+    {id: 1, url: '../assets/images/slider.png'},
+    {id: 2, url: '../assets/images/slider.png'},
+    {id: 3, url: '../assets/images/slider.png'},
+    {id: 4, url: '../assets/images/slider.png'},
+    {id: 5, url: '../assets/images/slider.png'},
+    {id: 6, url: '../assets/images/slider.png'},
+  ]
+
+  pictures3: any[] = [
+    {id: 1, url: '../assets/images/inicio9.jpg'},
+    {id: 2, url: '../assets/images/inicio10.jpg'},
+    {id: 3, url: '../assets/images/inicio11.jpg'},
+  ]
   constructor() {}
 
+  
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -6,7 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-
+  departments: any[] = [
+    {id: 1, name: "Mujer"},
+    {id: 2, name: "Hombre"},
+    {id: 3, name: "Bebés 0-24 meses"},
+    {id: 4, name: "Niños y Niñas"},
+    {id: 5, name: "Zapatos"},
+    {id: 6, name: "Belleza"},
+    {id: 7, name: "Relojes, Lentes y Joyeria"},
+    {id: 8, name: "Deportes"},
+    {id: 9, name: "Computo y electrónica"},
+    {id: 10, name: "Celulares"},
+    {id: 11, name: "Videojuegos"},
+    {id: 12, name: "Jugetes"},
+    {id: 13, name: "Línea Blanca"},
+    {id: 14, name: "Muebles"},
+    {id: 15, name: "Cocina"},
+    {id: 16, name: "Viajes"},
+    {id: 17, name: "Casa"},
+    {id: 18, name: "Vinos y Gourmet"},
+    {id: 19, name: "Hallowen y Navidad"},
+    {id: 20, name: "Otras Categorías"}
+  ]
   constructor() {}
 
 }
